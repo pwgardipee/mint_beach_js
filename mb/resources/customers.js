9db1324cfea0c1832c1b28b4ac8a39591c432b24
Peyton Gardipee <peytongardipee@gmail.com>
2021-09-06 13:37:36 -0700

    Add card interaction methods to customer resource.

--- mb/resources/customers.js
+++ mb/resources/customers.js
@@ -1,3 +1,6 @@
+//Package Imports
+const axios = require("axios").default;
+
 //Local Imports
 const resourceModel = require("./../models/resourceModel");
 
@@ -8,6 +11,45 @@ class Customers extends resourceModel {
    */
   constructor(tenantid) {
     super(tenantid, "customers");
+  }
+
+  /**
+   * Function to get all cards of a customer
+   * @param {String} id The customer's id
+   * @returns {Promise} The result of the API request
+   */
+  listCards(id) {
+    return axios.get(`${this.tenantid}/customers/${id}/cards`);
+  }
+
+  /**
+   * Function to get a card of a customer
+   * @param {String} id The customer's id
+   * @param {String} cardID The card's id
+   * @returns {Promise} The result of the API request
+   */
+  getCard(id, cardID) {
+    return axios.get(`${this.tenantid}/customers/${id}/cards/${cardID}`);
+  }
+
+  /**
+   * Function to get create a card for a customer
+   * @param {String} id The customer's id
+   * @param {String} payload Card related data (source, etc)
+   * @returns {Promise} The result of the API request
+   */
+  addCard(id, payload) {
+    return axios.post(`${this.tenantid}/customers/${id}/cards`, payload);
+  }
+
+  /**
+   * Function to delete a card from a customer
+   * @param {String} id The customer's id
+   * @param {String} cardID The card's id
+   * @returns {Promise} The result of the API request
+   */
+  removeCard(id, cardID) {
+    return axios.delete(`${this.tenantid}/customers/${id}/cards/${cardID}`);
   }
 }
 
